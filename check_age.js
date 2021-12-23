1415778717b0be973de0e1ea818fa3c0eacd3d7a
Nachtw8 <94112184+Nachtw8@users.noreply.github.com>
2021-12-23 11:44:07 +0100

    Add files via upload

---
+++ check_age.js
@@ -0,0 +1,44 @@
+let age = 19;
+let isFemale = false;
+let driverStatus = 'bob';
+let firstName='Bram';
+let totalAmount= 30;
+
+if (age > 18) {
+    console.log('The Person is allowed in');
+}  else {
+    console.log ('The person is too young and is kindly requested to stay outside');
+}
+
+/* Check if the person is a woman */
+
+if (isFemale) {
+    console.log ('The Person is a woman!');
+} else {
+    console.log ('The person is a man!');
+}
+
+if (driverStatus == 'bob') {
+    console.log ('The driver is the Bob!')
+} else {
+    console.log ('The person is not a Bob!')
+}
+
+if (age > 18 && age < 25) {
+    console.log('You get 50% Off!');
+}  else {
+    console.log ('You don not  get Off!');
+}
+
+if ( age > 18  || firstName == 'Bram') {
+ console.log( "Hoeray, yo get a free beer!")
+}
+
+if (totalAmount > 25 & totalAmount <= 50) {
+    console.log ('You get free bitterballen');
+} else if (totalAmount > 50 & totalAmount < 100) {
+        console.log ('You get portion of nachos');
+} else if (totalAmount > 100) {
+    console.log ('You get free bottle of champagne');
+} else if (totalAmount<25) { 
+    console.log("You have to drink more beer to get fee!!");}
